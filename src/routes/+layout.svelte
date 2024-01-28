cf260123bb0d61628ce34ba06e0eca7c81555490
harmaz <you@example.com>
2024-01-27 17:19:51 -0800

    finished navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,32 @@
 <script>
     import "../app.css";
     import loggedIn from "$lib/auth.js";
+    import { Navbar, NavBrand, NavLi, NavUl, NavHamburger, Button, Input } from 'flowbite-svelte';
 
     loggedIn();
 </script>
+
+
+
+<nav class="dark:bg-gray-700">
+    <div class="mx-auto p-4">
+        <ul class="flex md:space-x-8 md:flex-row md:border-0">
+          <li>
+            <a href="#" class="text-white">Home</a>
+          </li>
+          <li>
+            <a href="#" class="text-white">Profile</a>
+          </li>
+          <li>
+            <a href="#" class="text-white">Contact</a>
+          </li>
+        </ul>
+    </div>
+  </nav>
+
+
+
+
 <main class="mx-auto container">
         <slot></slot>
 </main>
